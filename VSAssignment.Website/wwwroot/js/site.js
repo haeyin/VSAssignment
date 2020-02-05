3aecaf67ab68c47af3306fb67e5a23d494633044
Clare Shin <haeyin.shin@gmail.com>
2020-02-04 20:06:40 -0800

    Add project files.

---
+++ VSAssignment.Website/wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+function musicNote() {
+    var elem = document.getElementById("animate");
+    var pos = 0;
+    var id = setInterval(frame, 5);
+    function frame() {
+        if (pos == 1000) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.left = pos + "px";
+        }
+    }
+
+};
